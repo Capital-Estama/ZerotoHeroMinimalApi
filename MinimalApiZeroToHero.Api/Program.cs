868d0039f10f5c8aed15745f75dad0dcba171a50
Capital-Estama <capitalestama@gmail.com>
2024-09-04 18:31:47 -0400

    Routing Requests Questions Added

--- MinimalApiZeroToHero.Api/Program.cs
+++ MinimalApiZeroToHero.Api/Program.cs
@@ -1,6 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("Get-example", () => "Hello from GET");
+app.MapPost("Post-Heavy", () => "Your Post Sir");
+
+// synchronous 
+app.MapGet("ok-object", () => Results.Ok(new
+{
+    Name = "Test Account"
+}));
+
+// Rewrite above as Async 
+
+
+
+// Write top 4 HTTP CRUD REST operations
+
+
+// Write  Route with MapMethods 
+
+
+// Write Handler coming from a Var 
+
+//Write a Class and pass MapGet a delegate 
 
 app.Run();
